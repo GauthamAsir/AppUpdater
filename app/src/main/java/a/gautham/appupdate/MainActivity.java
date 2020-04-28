97b61eea2addd07c3ff66eca934118a4b88ecbf7
GauthamAsir <gauthamasir@gmail.com>
2020-04-28 20:33:07 +0530

    app: misc
    Signed-off-by: GauthamAsir <gauthamasir@gmail.com>

--- app/src/main/java/a/gautham/appupdate/MainActivity.java
+++ app/src/main/java/a/gautham/appupdate/MainActivity.java
@@ -1,28 +1,35 @@
 package a.gautham.appupdate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import a.gautham.library.AppUpdater;
 import a.gautham.library.Display;
-import a.gautham.library.models.Update;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppUpdater appUpdater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppUpdater appUpdater = new AppUpdater(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        appUpdater = new AppUpdater(this);
         appUpdater.setDisplay(Display.DIALOG);
         appUpdater.setUpGithub("GauthamAsir","WhatsApp_Status_Saver");
         appUpdater.start();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        appUpdater.stop();
     }
 }
